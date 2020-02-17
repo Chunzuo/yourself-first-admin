a2618aba5ab1ea1663f0b958bc02067320c99877
Wang <Chunzuowang@gmail.com>
2020-02-17 17:39:40 +0800

    update axios logic

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,12 @@
 // axios
 import axios from "axios";
 
+var baseURL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:3000/api"
+    : "https://yourself-first.herokuapp.com/api";
+
 export default axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: baseURL
   // You can add your headers here
 });
